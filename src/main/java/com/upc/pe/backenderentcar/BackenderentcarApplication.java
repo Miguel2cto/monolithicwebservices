d12c93386d778a46ff4329100627b0fc1c2bab22
2CTO <tucto32@gmail.com>
2023-04-01 12:29:42 -0500

    fixing problems on CORS

--- src/main/java/com/upc/pe/backenderentcar/BackenderentcarApplication.java
+++ src/main/java/com/upc/pe/backenderentcar/BackenderentcarApplication.java
@@ -1,13 +1,11 @@
 package com.upc.pe.backenderentcar;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -25,7 +23,7 @@ public class BackenderentcarApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/")
-                        .allowedOrigins("http://localhost:4200");
+                        .allowedOrigins("https://localhost:4200");
             }
 
         };
